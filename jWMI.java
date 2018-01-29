0c359226550a206ec50203efd0164cc41dc701c3
Muneeb ul Hassan <smati.muneeb@gmail.com>
2018-01-29 13:58:34 +0100

    Include Logs

--- jWMI.java
+++ jWMI.java
@@ -1,5 +1,7 @@
 package com.citumpe.ctpTools;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -11,16 +13,15 @@ public class jWMI
 {
 	private static final String CRLF = "\r\n";
 	
-	/**
-	 * Generate a VBScript string capable of querying the desired WMI information.
-	 * @param wmiQueryString the query string to be passed to the WMI sub-system.
-	 * <br>i.e. "Select * from Win32_ComputerSystem"
-	 * @param wmiCommaSeparatedFieldName a comma separated list of the WMI fields to be collected from the query results.
-	 * <br>i.e. "Model"
-	 * @return the vbscript string.
+	static Logger logger = Logger.getLogger("myLogger");
+	/*
+	 * 
+	 * To get object class of the query
+	 * 
 	 * */
 	private static String getVBScript(String wmiQueryStr, String wmiCommaSeparatedFieldName)
 	{
+		
 		String vbs = "Dim oWMI : Set oWMI = GetObject(\"winmgmts:\")"+CRLF;
 		vbs += "Dim classComponent : Set classComponent = oWMI.ExecQuery(\""+wmiQueryStr+"\")"+CRLF;
 		vbs += "Dim obj, strData"+CRLF;
@@ -28,13 +29,14 @@ public class jWMI
 		String[] wmiFieldNameArray = wmiCommaSeparatedFieldName.split(",");
 		for(int i = 0; i < wmiFieldNameArray.length; i++)
 		{
+			
+
 			vbs += "  strData = strData & obj."+wmiFieldNameArray[i]+" & VBCrLf"+CRLF;
 		}
 		vbs += "Next"+CRLF;
 		vbs += "wscript.echo strData"+CRLF;
 		return vbs;
 	}
-	
 
 	private static String getEnvVar(String envVarName) throws Exception
 	{
@@ -47,11 +49,9 @@ public class jWMI
 		return envVarValue;
 	}
 	
-	/**
-	 * Write the given data string to the given file
-	 * @param filename the file to write the data to
-	 * @param data a String ofdata to be written into the file
-	 * @throws Exception if the output file cannot be written
+	/*
+	 * Write the query in tmp file to execute it
+	 * 
 	 * */
 	private static void writeStrToFile(String filename, String data) throws Exception
 	{
@@ -62,13 +62,7 @@ public class jWMI
 		output = null;
 	}
 	
-	/**
-	 * Get the given WMI value from the WMI subsystem on the local computer
-	 * @param wmiQueryStr the query string as syntactically defined by the WMI reference
-	 * @param wmiFieldName the field object that you want to get out of the query results
-	 * @return the value
-	 * @throws Exception if there is a problem obtaining the value
-	 * */
+
 	public static String getWMIValue(String wmiQueryStr, String wmiCommaSeparatedFieldName) throws Exception
 	{
 		String vbScript = getVBScript(wmiQueryStr, wmiCommaSeparatedFieldName);
@@ -81,12 +75,6 @@ public class jWMI
 		return output.trim();
 	}
 	
-	/**
-	 * Execute the application with the given command line parameters.
-	 * @param cmdArray an array of the command line params
-	 * @return the output as gathered from stdout of the process
-	 * @throws an Exception upon encountering a problem  
-	 * */
 	private static String execute(String[] cmdArray) throws Exception
 	{
 		Process process = Runtime.getRuntime().exec(cmdArray);
@@ -95,7 +83,7 @@ public class jWMI
 		String line = "";
 		while((line = input.readLine()) != null)
 		{
-			//need to filter out lines that don't contain our desired output
+			
 			if(!line.contains("Microsoft") && !line.equals(""))
 			{
 				output += line +CRLF;
@@ -106,37 +94,44 @@ public class jWMI
 		return output.trim();
 	}
 	
-	public static void executeDemoQueries(String query)
+	public static void executeDemoQueries()
 	{
 		try
 		{
-			System.out.println(getWMIValue("Select * from Win32_ComputerSystem", "Model"));
-			System.out.println(getWMIValue("Select Name from Win32_ComputerSystem", "Name"));
+			System.out.println("Model: "+getWMIValue("Select * from Win32_ComputerSystem", "Model"));
+			System.out.println("Name: "+getWMIValue("Select Name from Win32_ComputerSystem", "Name"));
+			
 			//System.out.println(getWMIValue("Select Description from Win32_PnPEntity", "Description"));
 			//System.out.println(getWMIValue("Select Description, Manufacturer from Win32_PnPEntity", "Description,Manufacturer"));
 			//System.out.println(getWMIValue("Select * from Win32_Service WHERE State = 'Stopped'", "Name"));
 			//this will return everything since the field is incorrect and was not used to a filter
 			//System.out.println(getWMIValue("Select * from Win32_Service", "Name"));
 			//this will return nothing since there is no field specified
-			System.out.println(getWMIValue("Select Name from Win32_ComputerSystem", ""));
-			//this is a failing case where the Win32_Service class does not contain the 'Name' field
-			System.out.println(getWMIValue("Select * from Win32_DiskDrive", "DeviceID,SerialNumber,PNPDeviceID,Model,Manufacturer,MediaType,FirmwareRevision,DeviceID,Partitions,Size"));
-			//System.out.println(getWMIValue("Select * from Win32_VideoController Where DeviceID='VideoController1'","PNPDeviceID, DriverVersion"));
+			//System.out.println(getWMIValue("Select Name from Win32_ComputerSystem", ""));
+			
+			System.out.println("Disk Drive DeviceID:"+getWMIValue("Select * from Win32_DiskDrive", "DeviceID"));
+			System.out.println("Disk Drive Serial Number:"+getWMIValue("Select * from Win32_DiskDrive", "SerialNumber"));
+			System.out.println("Disk Drive Model:"+getWMIValue("Select * from Win32_DiskDrive", "Model"));
+			System.out.println("Disk Drive Manufacturere:"+getWMIValue("Select * from Win32_DiskDrive", "Manufacturer"));
+			System.out.println("Video Controller Name:"+getWMIValue("Select * from Win32_VideoController Where DeviceID='VideoController1'","Name"));
+			System.out.println("Network Name:"+getWMIValue("SELECT * FROM Win32_NetworkAdapter WHERE DeviceID='10' OR DeviceID = '11'","Name"));
+			System.out.println("Network MACAddress:"+getWMIValue("SELECT * FROM Win32_NetworkAdapter WHERE DeviceID='10' OR DeviceID = '11'","MACAddress"));
 		}
 		catch(Exception e)
 		{
+			logger.log(Level.SEVERE, "Cannot reach the function body");
 			e.printStackTrace();
 		}
 	}
 	
 	public static void main(String[] argv)
 	{
+		Logger logger = Logger.getLogger("myLogger");
 		try
 		{
-			getWMIValue("Select * from Win32_ComputerSystem", "Model");
-			//	executeDemoQueries(query);
-			
-			
+			logger.log(Level.INFO, "Starting to Get the data From WMI");
+			//getWMIValue("Select * from Win32_ComputerSystem", "Model");
+				executeDemoQueries();
 		} 
 		catch(Exception e)
 		{
